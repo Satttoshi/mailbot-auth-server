653d8cc8dab6a9cfe18af264b459cc79318bfb72
Joshua Stolle <joshuastolle445@gmail.com>
2024-05-11 18:44:13 +0200

    feat: user-api routes and handlers
    This commit adds functionality to create and retrieve users to/from MongoDB. It introduces two new HTTP routes ('/create_user' and '/get_user/{username}') with corresponding handlers, enhancing the web API's capability. Modifications were also made to the .env file to adjust the MongoDB URI.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 mod db;
 
 use actix_web::{web, App, HttpServer, Responder, HttpResponse};
-use db::create_mongo_client;
+use mongodb::Client;
+use db::{create_mongo_client, create_user, find_user_by_username, User};
 
 async fn index() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
@@ -10,13 +11,33 @@ async fn index() -> impl Responder {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let mongo_client = create_mongo_client().await.expect("Failed to connect to MongoDB");
+    let mongo_data = web::Data::new(mongo_client);
 
     HttpServer::new(move || {
         App::new()
-            .app_data(mongo_client.clone()) // Pass the client to the application
+            .app_data(mongo_data.clone()) // Pass the client to the application
             .route("/", web::get().to(index))
+            .route("/create_user", web::post().to(create_user_handler))
+            .route("/get_user/{username}", web::get().to(get_user_handler))
     })
         .bind("127.0.0.1:8080")?
         .run()
         .await
+}
+
+async fn create_user_handler(data: web::Data<Client>, web::Json(user_data): web::Json<User>) -> impl Responder {
+    let result = create_user(&data, &user_data.username, &user_data.email , &user_data.password).await;
+    match result {
+        Ok(_) => HttpResponse::Created().body("User created successfully"),
+        Err(e) => HttpResponse::InternalServerError().body(e.to_string()),
+    }
+}
+
+async fn get_user_handler(data: web::Data<Client>, username: web::Path<String>) -> impl Responder {
+    let result = find_user_by_username(&data, &username).await;
+    match result {
+        Ok(Some(user)) => HttpResponse::Ok().json(user),
+        Ok(None) => HttpResponse::NotFound().body("User not found"),
+        Err(e) => HttpResponse::InternalServerError().body(e.to_string()),
+    }
 }
