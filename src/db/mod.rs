05ed24c259a73b47d71919c183b486f184747bf3
Joshua Stolle <joshuastolle445@gmail.com>
2024-05-11 16:36:47 +0200

    feat: user management functionality in MongoDB
    This commit introduces User struct and functions to create and find a user in MongoDB. User struct provides fields such as username, email, and password. New environment variable 'MONGO_DB_NAME' has also been added to specify the database name.

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -1,6 +1,8 @@
-use mongodb::{Client, options::ClientOptions};
+use mongodb::{Collection, Client, options::ClientOptions};
 use std::env;
 use dotenv::dotenv;
+use mongodb::bson::doc;
+use serde::{Deserialize, Serialize};
 
 pub async fn create_mongo_client() -> Result<Client, mongodb::error::Error> {
     dotenv().ok(); // Load .env file if present
@@ -8,4 +10,35 @@ pub async fn create_mongo_client() -> Result<Client, mongodb::error::Error> {
     let client_options = ClientOptions::parse(&db_uri).await?;
     let client = Client::with_options(client_options)?;
     Ok(client)
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct User {
+    pub username: String,
+    pub email: String,
+    pub password: String,
+}
+
+pub fn get_user_collection(client: &Client) -> Collection<User> {
+    let db_name = env::var("MONGO_DB_NAME").expect("You must set the MONGO_DB_NAME environment variable");
+    let db = client.database(&db_name);
+    db.collection::<User>("users")
+}
+
+pub async fn create_user(client: &Client, username: &str, email: &str, password: &str) -> mongodb::error::Result<()> {
+    let user_collection = get_user_collection(client);
+    let new_user = User {
+        username: username.to_string(),
+        email: email.to_string(),
+        password: password.to_string(),
+    };
+    user_collection.insert_one(new_user, None).await?;
+    Ok(())
+}
+
+pub async fn find_user_by_username(client: &Client, username: &str) -> mongodb::error::Result<Option<User>> {
+    let user_collection = get_user_collection(client);
+    let filter = doc! { "username": username };
+    let user = user_collection.find_one(filter, None).await?;
+    Ok(user)
 }
